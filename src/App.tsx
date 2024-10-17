8457bea6773701af92dee9be7a66c786029703e0
Mateus Fernandes <mateusveloso2608@gmail.com>
2024-10-17 11:15:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import logoImg from "./assets/logo.png";
+import { Search } from "./components/search.tsx";
+
+interface UserProps {
+  name: string;
+  bio: string;
+  avatar_url: string;
+  login: string;
+  followers: number;
+  following: number;
+}
+
+export function App() {
+  const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const loadUser = async (username: string) => {
+    setLoading(true);
+    setError(false);
+    setUser(null);
+    
+    try {
+      const res = await fetch(`https://api.github.com/users/${username}`);
+      const data = await res.json();
+
+      if (res.status === 404) {
+        setError(true);
+        setLoading(false);
+        return;
+      }
+
+      const { name, bio, avatar_url, login, followers, following } = data;
+      const userData: UserProps = {
+        name,
+        bio,
+        avatar_url,
+        login,
+        followers,
+        following,
+      };
+      setUser(userData);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="w-full h-screen mx-auto flex flex-col items-center  bg-indigo-900 overflow-y-scroll">
+      <img src={logoImg} alt="logo do github" className="w-40 mx-auto my-6" />
+
+      <div className="flex flex-col w-full max-w-2xl gap-4 my-2 bg-indigo-700   text-white rounded-lg p-4">
+        <Search loadUser={loadUser}/>
+      </div>
+
+      {loading && <p className="text-white mt-4">Carregando...</p>}
+
+      {error && (
+        <p className="text-red-500 mt-4">
+          Usuário não encontrado. Tente novamente.
+        </p>
+      )}
+
+      {user && (
+        <div className="flex flex-col w-full max-w-2xl gap-4  bg-indigo-700 my-6 rounded-lg p-8">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <img
+              src={user.avatar_url}
+              alt="foto de perfil do usuário"
+              className="w-1/3 border-2 border-white rounded-full hover:scale-105 transition-all"
+            />
+            <h2 className="text-2xl text-white">{user.name}</h2>
+            <p className="text-lg text-white">
+              Usuário: {user.login}
+            </p>
+            <p className="text-lg text-white">{user.bio}</p>
+
+            <div className="flex gap-6 items-center justify-center">
+              <p className="font-medium text-white">Seguidores: {user.followers}</p>
+              <p className="font-medium text-white">Seguindo: {user.following}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/search.tsx/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+interface SearchProps {
+  loadUser: (username: string) => Promise<void>;
+}
+
+export function Search({ loadUser }: SearchProps) {
+  const [username, setUsername] = useState("");
+
+  return (
+    <div className="flex w-full flex-col justify-center items-center gap-4 p-8">
+      <h2 className="text-2xl">Pesquise por um usuário:</h2>
+      <input
+        type="text"
+        placeholder="Digite o nome do usuário"
+        onChange={(e) => setUsername(e.target.value)}
+        className="w-96 p-2 rounded-md  text-black outline-none"
+      />
+      <button
+        onClick={() => loadUser(username)}
+        className="bg-white px-8 py-2 rounded-lg text-black hover:bg-slate-200 transition-all "
+      >
+        Buscar
+      </button>
+    </div>
+  );
+}
